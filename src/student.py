0081244b466107340f5375d8aa160b858bb3d1a4
Michiel De Roeck <michiel.deroeck@ugent.be>
2023-04-28 18:00:11 +0200

    todo

--- src/student.py
+++ src/student.py
@@ -5,3 +5,16 @@ class Student:
     def load_student_data(self, student_name):
         # open json file van student en stokeer de huidige correcties
         pass
+
+    def remove_comment(self, question_id, comment_id):
+        pass
+
+    def add_comment(self, question_id, comment_id):
+        '''
+        - questions should have an id: "0", "1", ... (ordereddict <template>)
+            - init_Student: add structure directly (new student -> "0":[], "1":[] ...)
+
+        '''
+        pass
+
+
